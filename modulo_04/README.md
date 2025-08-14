3757158838041e0f8ea95ac96eed83d7b7b16b32
Blimbliem <studyblimblim@gmail.com>
2025-08-14 12:12:16 -0300

    métodos string

---
+++ modulo_04/README.md
@@ -0,0 +1,4 @@
+## Strings
+### Métodos uteis
+- Em Python a classe string tem vários métodos e utilitários que a tornam uma linguagem mais fácil de se trabalhar.
+
---
+++ modulo_04/metodos.py
@@ -0,0 +1,16 @@
+curso = "python"
+
+print(curso.upper())
+print(curso.lower())
+print(curso.title())
+
+curso2 = "    Python "
+print(curso2.strip())
+print(curso2.lstrip())
+print(curso2.rsplit())
+
+curso3 = "Python"
+
+print(curso3.center(10,"#"))
+print(".".join(curso3))
+
