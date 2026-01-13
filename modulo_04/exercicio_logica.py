09e6b70aac068bec6216a79c0c91eae825eb1db4
Blimbliem <studyblimblim@gmail.com>
2026-01-13 14:37:47 -0300

    trazendo arquivo de exercicio

---
+++ modulo_04/exercicio_logica.py
@@ -0,0 +1,22 @@
+def aumento_salario(salario_atual,nome):
+    if salario_atual >= 5000:
+                aumento = 0.10
+    else:
+        aumento = 0.15
+
+    novo_salario = salario_atual +(salario_atual*aumento)
+    print(f"Olá {nome}, seu novo salário é R$ {novo_salario:.2f}. "
+                f"Seu aumento foi de {aumento*100}%!")
+
+contador = 0 
+while contador < 3:
+    print(f"\n--- Funcionário {contador + 1} ---")
+    nome = input("Digite o nome do funcionario: ")
+    salario_atual = float(input("Digite seu salario: "))
+
+    aumento_salario(salario_atual, nome)    
+
+    contador = contador + 1
+
+    print("Todos funcionarios foram processados!")
+
