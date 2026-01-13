1900167881a64c86f6a76ca255224d152bd28931
Blimbliem <studyblimblim@gmail.com>
2026-01-13 14:13:13 -0300

    Organizando branch listas: mantendo apenas o modulo 05

--- modulo_02/operadores_identidade.py
+++
@@ -1,9 +0,0 @@
-curso = "Curso de Python"
-nome_curso = curso
-saldo, limite = 200,200
-
-conf1 = curso is nome_curso
-conf2 = curso is not nome_curso
-conf3 = saldo is limite
-
-print(conf1,conf2, conf3)
--- modulo_03/estrutura_condicional_if_ternario.py
+++
@@ -1,6 +0,0 @@
-saldo = 1200.0
-saque = float(input("Quanto você deseja sacar?"))
-
-status = "Sucesso" if saldo >= saque else "Falha"
-
-print(f"{status} ao realizar o saque! Você sacou {saque}.")
